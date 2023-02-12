235f47ba48e909fe52962dea49fa404031ce914c
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-02-12 20:50:29 +0100

    feat: working dynamic route reading data from md

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,28 +1,53 @@
-import Head from "next/head"
-import Image from 'next/image'
-import { attributes, react as HomeContent } from '../../../public/_posts/blog/content.md'
+import { FC} from 'react'
+import fs from 'fs'
+import ReactMarkdown from 'react-markdown'
+import matter from 'gray-matter'
 
-const Blog = () => {
-
-    const { title, thumbnail, date, rating } = attributes
-
-    return (
-      <>
-        <Head>
-          <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
-        <article>
-          <h1>{title}</h1>
-          <h2>{date}</h2>
-          <p>{rating}</p>
-          <HomeContent />
-          <ul>
-            <Image alt='' width='120' height='100' src={`/..${thumbnail}`} />
-          </ul>
-        </article>
-      </>
-    )
-  
+type BlogProps = {
+  frontmatter: {
+    layout: string
+    title: string
+    date: string
+    thumbnail: string
+    rating: number
+  }
+  markdown: string
+}
+const Blog:FC<BlogProps> = ({ frontmatter, markdown}) => {
+  const {title, date, rating } = frontmatter
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h3>{date}</h3>
+      <p>{rating}</p>
+      <ReactMarkdown>
+        {markdown}
+      </ReactMarkdown>
+    </div>
+  )
 }
 
-export default Blog
+export async function getStaticProps({ params: { slug } }: {params: {slug: string}}) {
+  const fileContent = matter(fs.readFileSync(`./public/posts/blog/${slug}.md`, 'utf8'))
+
+  const {date, ...frontmatter} = fileContent.data
+  const markdown = fileContent.content
+
+  return {
+    props: { frontmatter: {...frontmatter, date: `${date}`}, markdown }
+  }
+}
+
+export async function getStaticPaths() {
+  const filesInProjects = fs.readdirSync('./public/posts/blog/')
+
+  const paths = filesInProjects.map(file => {
+    const filename = file.slice(0, file.indexOf('.'))
+    return { params: { slug: filename }}
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
