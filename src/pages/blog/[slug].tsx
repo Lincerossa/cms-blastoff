f6bce88775fbd2c39667dc0db3557ad3e89fcbb8
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-02-18 18:53:41 +0100

    feat: blogPage micro improvement mvp

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -2,36 +2,51 @@ import { FC} from 'react'
 import fs from 'fs'
 import ReactMarkdown from 'react-markdown'
 import matter from 'gray-matter'
-import { PostHero } from '@/components'
+import { Background, PostHero, Padder, Wrapper } from '@/components'
+import getFormattedDate from '@/helpers/utils/getFormattedDate'
+import theme from '@/helpers/style/theme'
 
 type BlogProps = {
   frontmatter: {
-    layout: string
     title: string
+    subtitle: string
     date: string
     thumbnail: string
     tags: string[]
   }
   markdown: string
 }
-const Blog:FC<BlogProps> = ({ frontmatter, markdown}) => {
-  const {title, date, tags, thumbnail } = frontmatter
+const BlogPage:FC<BlogProps> = ({ frontmatter, markdown}) => {
+  const {title, date, subtitle, tags, thumbnail } = frontmatter
   return (
-    <div>
-      <PostHero supertitle={date} title={title} subtitle={'subtitle'} media={{url:`${thumbnail.replace('/public','')}`}}/>
-      <ReactMarkdown>
-        {markdown}
-      </ReactMarkdown>
-    </div>
+    <>
+      <PostHero
+        supertitle={date}
+        title={title}
+        subtitle={subtitle}
+        media={{url:`${thumbnail.replace('/public','')}`}}
+      />
+      <Background color='white' background={theme.colors.thirdary}>
+        <Wrapper>
+          <Padder size='large'>
+              <ReactMarkdown>
+                {markdown}
+              </ReactMarkdown>
+          </Padder>
+        </Wrapper>
+      </Background>
+    </>
   )
 }
-export default Blog
+export default BlogPage
 
 export async function getStaticProps({ params: { slug } }: {params: {slug: string}}) {
   const { data, content } = matter(fs.readFileSync(`./public/posts/blog/${slug}.md`, 'utf8'))
 
+
+
   return {
-    props: { frontmatter: {...data, date: `${data.date}`}, markdown: content }
+    props: { frontmatter: {...data, date: getFormattedDate(new Date(data.date))}, markdown: content }
   }
 }
 
