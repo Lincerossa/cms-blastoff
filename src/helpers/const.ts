f18fa2ccdedd02de4c73d734d9344c8483ee9fc1
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-02-19 19:59:38 +0100

    feat: layout integrated in _app

--- src/helpers/const.ts
+++ src/helpers/const.ts
@@ -3,9 +3,14 @@ export const SETTINGS = {
     title: 'cms-blastoff',
     description: 'cms-blastoff: a beautiful headless starter cms',
     image: '/cms-blastoff.jpg',
-  }
+  },
+  ROUTES: [{
+    hidden: false,
+    slug: '/blog',
+    href: '/blog',
+    label: 'Blog'
+  }]
 }
-
 export const DATA = {
   intro: `A static CMS (Content Management System) is a type of CMS that generates websites using static files, typically in HTML, CSS, and JavaScript, instead of relying on a server-side dynamic rendering system. In a static CMS, the content is stored in files that are generated ahead of time, and when a user requests the site, the files are served as-is. This is in contrast to traditional CMS systems, where content is stored in a database and generated dynamically at the time of the request.
 
