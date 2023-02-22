e4d0faabe9e5b9180d1a9e897f53f01cae2bf09e
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-02-22 18:39:57 +0100

    feat: listOfCards pagination installed

---
+++ src/components/ListOfCards/hooks.ts
@@ -0,0 +1,35 @@
+import { useState, useMemo } from 'react'
+import { UseListPagination, SelectedCategory } from './types'
+
+export const useListPagination: UseListPagination = ({items, itemsPerPage}) => {
+  const [selectedCategory, selectCategory] = useState<SelectedCategory>()
+  const [itemOffset, setItemOffset] = useState(0)
+
+  const handlePageClick = (event: { selected: number }) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length
+    setItemOffset(newOffset)
+  }
+  const handleCategoryClick = (category: SelectedCategory) => {
+    setItemOffset(0)
+    selectCategory(category)
+  }
+
+  const { currentItems, pageCount, selectedPage } = useMemo(() => {
+    const filteredItems = items.filter(e => selectedCategory ? e.category === selectedCategory : true )
+
+    return {
+      currentItems: filteredItems.slice(itemOffset, itemOffset + itemsPerPage),
+      pageCount: Math.ceil(filteredItems.length / itemsPerPage),
+      selectedPage: itemOffset / itemsPerPage
+    }
+  }, [itemOffset, items, itemsPerPage, selectedCategory])
+
+  return {
+    itemOffset,
+    pageCount,
+    currentItems,
+    handlePageClick,
+    handleCategoryClick,
+    selectedPage
+  }
+}
--- src/components/ListOfCards/types.ts
+++ src/components/ListOfCards/types.ts
@@ -4,3 +4,13 @@ export type ListOfCardsProps = {
   itemsPerPage?: number
   items: Array<CardProps & {slug: string}>
 }
+
+export type SelectedCategory = 'blog' | 'project' | undefined
+
+export type UseListPagination = (params: {items:ListOfCardsProps['items'], itemsPerPage:number}) => {
+  pageCount: number
+  selectedPage: number
+  currentItems: ListOfCardsProps['items']
+  handlePageClick: (params:{selected: number}) => void
+  handleCategoryClick: (params: SelectedCategory) => void
+}
